124408c51f16d9b346cec09688a7ba502697b2b1
SURAJSPS <76001600+SURAJSPS@users.noreply.github.com>
2021-06-01 13:24:57 +0530

    user animal viwe

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,9 +2,12 @@
 // Generated file. Do not edit.
 //
 
+import 'package:another_flushbar/another_flushbar_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -13,9 +16,12 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  AnotherFlushbarWeb.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
