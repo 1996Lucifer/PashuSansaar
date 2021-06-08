0b475c761357be38c284657f21e23f07e19ab679
SURAJSPS <76001600+SURAJSPS@users.noreply.github.com>
2021-06-08 23:44:38 +0530

    login comp

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,4 @@
-import 'package:pashusansaar/login_screen.dart';
+import 'package:pashusansaar/domain/auth/login/login_screen.dart';
 import 'package:pashusansaar/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
